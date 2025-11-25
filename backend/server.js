f8b0ed38f4127251a151c9d65a133c057997b634
Nikittaaaaa <162281255+Nikittaaaaa@users.noreply.github.com>
2025-11-25 12:06:36 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -41,19 +41,3 @@ app.listen(PORT, () => {
 	console.log("Server is running on http://localhost:" + PORT);
 	connectDB();
 });
-const express = require('express');
-const path = require('path');
-const app = express();
-
-// ===== API маршрути тут =====
-
-// віддавати React build
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server running...');
-});
